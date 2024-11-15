ae27041dfabb0e81a00a853def878376020e7dbd
Yevhenii <146197566+Yevhenii1488@users.noreply.github.com>
2024-11-15 12:47:55 +0200

    Add a folder for homework1

---
+++ first home task/simple_calendar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("   January 2024\n");
+    printf("Su Mo Tu We Th Fr Sa\n");
+    printf("    1  2  3  4  5  6\n");
+    printf(" 7  8  9 10 11 12 13\n");
+    printf("14 15 16 17 18 19 20\n");
+    printf("21 22 23 24 25 26 27\n");
+    printf("28 29 30 31\n");
+    return 0;
+}
